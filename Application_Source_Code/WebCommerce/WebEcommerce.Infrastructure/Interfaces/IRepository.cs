f4ec454c21d592b177aeb0fb947d32d889f7ec30
nucasspro <hailongdang.hailong@gmail.com>
2019-11-25 22:32:02 +0700

    Add Repository and UnitOfWork interfaces

---
+++ Application_Source_Code/WebCommerce/WebEcommerce.Infrastructure/Interfaces/IRepository.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace WebEcommerce.Infrastructure.Interfaces
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="T">T is entity class/entity</typeparam>
+    /// <typeparam name="K">K is type</typeparam>
+    public interface IRepository<T, K> where T : class
+    {
+        T GetById(K id, params Expression<Func<T, object>>[] includeProperties);
+
+        T GetSingle(Expression<Func<T, bool>> predicate, params Expression<Func<T, object>>[] includeProperties);
+
+        IQueryable<T> GetAll(params Expression<Func<T, object>>[] includeProperties);
+
+        IQueryable<T> GetAll(Expression<Func<T, bool>> predicate, params Expression<Func<T, object>>[] includeProperties);
+
+        void Add(T entity);
+
+        void Update(T entity);
+
+        void Remove(T entity);
+
+        void Remove(K id);
+
+        void RemoveMultiple(List<T> entities);
+    }
+}
---
+++ Application_Source_Code/WebCommerce/WebEcommerce.Infrastructure/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace WebEcommerce.Infrastructure.Interfaces
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        void Commit();
+        Task CommitAsync();
+    }
+}
